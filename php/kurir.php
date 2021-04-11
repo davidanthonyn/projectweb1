943a0b16d767fa505639c4c5ee009b17855d2601
ignasiusmigel <brocoy1990@gmail.com>
2021-04-11 22:54:47 +0700

    Create kurir.php

---
+++ php/kurir.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Test</title>
+</head>
+<body>
+ <td><select name="Kurir">
+                    <option value="-">- Pilih Kurir -
+                    <option value="JNE">JNE
+                    <option value="SiLambat">SiLambat
+                    <option value="JNI">JNI
+                    <option value="A">A
+                </select></td>
+        </tr>
+
+<br><input type="submit"> 
+
+</form>
+</div>
+</body>
+</html>
