d8b3d3c00fb73102fbd2d9067f35f082ce2841d0
davidanthonyn <55152980+davidanthonyn@users.noreply.github.com>
2021-04-21 19:38:28 +0700

    404 in home

---
+++ 404page.php
@@ -0,0 +1,14 @@
+<?php 
+include "sigadgetheadererror.php";
+?>
+<html>
+<head>
+</head>
+<body>
+<center>
+<br>
+<img src = "https://drive.google.com/uc?export=view&id=1F4p7RJT5bG8orlMfOsDxT_AwEIeH8X-K" width="25%"><br><br>
+<h1>Oops, the page is not available.</h1>
+</center>
+</body>
+</html>
