85e6979428683adf7de34be9e935de96dc91a031
davidanthonyn <55152980+davidanthonyn@users.noreply.github.com>
2021-04-25 02:46:22 +0700

    Update sigadgetheadercustomer.php

--- Bagian David/sigadgetheadercustomer.php
+++ Bagian David/sigadgetheadercustomer.php
@@ -266,7 +266,7 @@ body {
 																					<li><a class="dropdown" href="../Bagian Migel/realme.php">Realme</a></li>
 																				</ul>
                                   </li>
-																																		<li><a href="../Bagian David/accessoriesproducts.php">Aksesoris</a>
+																																		<li><a href="../Bagian Tius/accessoriesproducts.php">Aksesoris</a>
 																																		<ul class="dropdown-list">
 																																				<li><a class="dropdown" href="../Bagian Tius/headphone.php">Headphone</a></li>
 																																				<li><a class="dropdown" href="../Bagian Tius/wirelesscharger.php">Powerbank</a></li>
