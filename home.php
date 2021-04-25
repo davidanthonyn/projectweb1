a363eeeb1d35394d1a6ee2b4786910c710677e45
davidanthonyn <55152980+davidanthonyn@users.noreply.github.com>
2021-04-26 00:53:05 +0700

    Update home.php

--- home.php
+++ home.php
@@ -65,12 +65,7 @@ $sqleight = mysqli_query($conn, "SELECT Filename FROM image WHERE Filename='New-
 																					<li><a class="dropdown" href="Bagian Migel/realme.php">Realme</a></li>
 																				</ul>
                                   </li>
-																																		<li><a href="Bagian David/accessoriesproducts.php">Aksesoris</a>
-																																		<ul class="dropdown-list">
-																																				<li><a class="dropdown" href="Bagian Tius/headphone.php">Headphone</a></li>
-																																				<li><a class="dropdown" href="Bagian Tius/wirelesscharger.php">Powerbank</a></li>
-																																				<li><a class="dropdown" href="Bagian Tius/aksesorislain.php">Aksesoris Lain</a></li>
-																																			</ul>
+																																		<li><a href="Bagian Tius/headphoneproducts.php">Headphone</a>
                                                                     </li>
 
 																																																			<li><a href="Bagian Sanctus/eventpromo.php">Event and Promo</a>
@@ -105,7 +100,7 @@ $sqleight = mysqli_query($conn, "SELECT Filename FROM image WHERE Filename='New-
 																																																						if(empty($_SESSION['account_username'])) {
 																																																							} else if(!empty($_SESSION['account_username'])) {
 																																																									if(!empty($_SESSION['account_userlevel']) && $_SESSION['account_userlevel']=='admin') {
-																																																								echo "<a href='sigadgetdashboard.php'>Dashboard</a>";
+																																																								echo "<a href='Bagian Sanctus/sigadgetdashboard.php'>Dashboard</a>";
 																																																								echo "<ul class='dropdown-list'>";
 																																																								echo "<li><a class='dropdown' href='sigadgettransactions.php'>Transactions</a></li>";
 																																																								echo "<li><a class='dropdown' href='Bagian Sanctus/sigadgetproducts.php'>Products</a></li>";
